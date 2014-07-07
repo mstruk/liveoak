acc8ccda8c2483ef6067445b30e1a37b46b68d85
Marko Strukelj <marko.strukelj@gmail.com>
2014-07-07 11:13:16 +0200

    Improve error handling when application deployment fails

--- modules/container/src/main/java/io/liveoak/container/tenancy/InternalApplicationExtension.java
+++ modules/container/src/main/java/io/liveoak/container/tenancy/InternalApplicationExtension.java
@@ -4,6 +4,7 @@ import io.liveoak.container.tenancy.service.ApplicationExtensionRemovalService;
 import io.liveoak.spi.LiveOak;
 import io.liveoak.spi.extension.Extension;
 import io.liveoak.spi.resource.async.Resource;
+import org.jboss.logging.Logger;
 import org.jboss.msc.service.AbstractServiceListener;
 import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceRegistry;
@@ -17,6 +18,8 @@ import java.util.function.Consumer;
  * @author Ken Finnigan
  */
 public class InternalApplicationExtension implements Consumer<Exception> {
+
+    private static Logger log = Logger.getLogger(InternalApplicationExtension.class);
 
     public InternalApplicationExtension(ServiceRegistry registry, InternalApplication app, String extensionId, String resourceId) {
         this.registry = registry;
@@ -94,6 +97,7 @@ public class InternalApplicationExtension implements Consumer<Exception> {
     @Override
     public void accept(Exception e) {
         this.exception = e;
+        log.error("Exception during application initialization (/" + app.id() + "/" + resourceId + "): ", e);
     }
 
     public Exception exception() {
